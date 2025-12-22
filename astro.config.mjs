a7e576f069d790085fafb2d996b1431c29167aff
Samir Fernandez <safernandez@viva.com.do>
2025-12-22 17:33:11 -0400

    Modificar y mejorar el landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/static";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({}),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
